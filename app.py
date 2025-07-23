222380c298ac1d2401375d51677b9edab85c76d5
Guilherme Castro <gui.castro.0001.1426@gmail.com>
2025-07-23 17:26:55 -0300

    add guvicorn

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from flask import Flask,render_template, Blueprint
+from flask import Flask, render_template, Blueprint
 import sqlite3
 import os 
 
 app = Flask(__name__)
 
+# importa e registra blueprint
 from route.formAdm import admin_bp
 app.register_blueprint(admin_bp)
 
 DATABASE = 'database.db'
 
-
 @app.route('/')
 def home():   
     return render_template('index.html')
 
+# Este bloco só serve localmente, ignora no Gunicorn
 if __name__ == '__main__':
     app.run(debug=True)
